a3333979b0deef5505b554c95f16e1e02327b2dc
Ross Rothenstine <rossrothenstine@gmail.com>
2016-07-17 01:58:25 -0700

    Initial draft.

---
+++ stress.go
@@ -0,0 +1,92 @@
+package stress
+
+import (
+	"sync"
+	"time"
+
+	"golang.org/x/net/context"
+	"golang.org/x/time/rate"
+)
+
+// A Worker does a unit of work towards the stress test.
+type Worker interface {
+	// Work should perform the unit of work for this worker.
+	// The context provided is shared amongst all workers, and is derived
+	// from the Stresser's context before calling Stresser.Start().
+	Work(context.Context)
+}
+
+type Stresser struct {
+	// Context is a shared state amongst all workers.
+	Context context.Context
+
+	// New allocates a new worker.
+	New func() Worker
+
+	Limit *rate.Limiter
+
+	workers []Worker
+	close   chan struct{}
+}
+
+func (s *Stresser) Start() {
+	s.close = make(chan struct{})
+	if s.Context == nil {
+		s.Context = context.TODO()
+	}
+	// Always allocate at least one worker
+	w := s.New()
+	s.workers = []Worker{w}
+
+	for {
+		select {
+		case <-s.close:
+			return
+		default:
+			s.doWork()
+		}
+	}
+}
+
+func (s *Stresser) doWork() {
+	timer := time.NewTimer(time.Second)
+	defer timer.Stop()
+
+	events := 0
+	for {
+		select {
+		case <-timer.C:
+			if events < int(s.Limit.Limit()) {
+				w := s.New()
+				s.workers = append(s.workers, w)
+			}
+			return
+		default:
+			wg := sync.WaitGroup{}
+			wg.Add(len(s.workers))
+			// Run a single unit of work on all workers
+			for _, w := range s.workers {
+				go func() {
+					defer wg.Done()
+					r := s.Limit.Reserve()
+					if r.OK() {
+						ok := s.Limit.Allow()
+						if !ok {
+							r.Cancel()
+							return
+						}
+					}
+					time.Sleep(r.Delay())
+					w.Work(s.Context)
+					events++
+				}()
+			}
+			wg.Wait()
+
+		}
+	}
+}
+
+func (s *Stresser) Stop() {
+	s.close <- struct{}{}
+}
---
+++ stress_test.go
@@ -0,0 +1,98 @@
+package stress_test
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"gitlab.com/Butabah/stress"
+	"golang.org/x/net/context"
+	"golang.org/x/time/rate"
+)
+
+type emptyWorker struct {
+}
+
+func (_ emptyWorker) Work(context.Context) {
+}
+
+func TestAllocatesWorkersFromPool(t *testing.T) {
+	called := false
+	stresser := &stress.Stresser{
+		New: func() stress.Worker {
+			called = true
+			return emptyWorker{}
+		},
+		Limit: rate.NewLimiter(10, 10),
+	}
+
+	go stresser.Start()
+	defer stresser.Stop()
+
+	timer := time.NewTimer(time.Second)
+	<-timer.C
+
+	if called == false {
+		t.Error("expected true, got false")
+	}
+}
+
+type workTracker struct {
+	numCalls int
+}
+
+func (w *workTracker) Work(context.Context) {
+	w.numCalls++
+}
+
+func TestLowerBound(t *testing.T) {
+	worker := &workTracker{}
+	stresser := &stress.Stresser{
+		New: func() stress.Worker {
+			return worker
+		},
+		Limit: rate.NewLimiter(20, 20),
+	}
+
+	go stresser.Start()
+	defer stresser.Stop()
+
+	timer := time.NewTimer(time.Second)
+	<-timer.C
+
+	if worker.numCalls < 10 {
+		t.Errorf("incorrect number of calls, expected at least %d, got %d", 10, worker.numCalls)
+	}
+}
+
+type slowWorker struct {
+	delay time.Duration
+}
+
+func (w *slowWorker) Work(context.Context) {
+	time.Sleep(w.delay)
+}
+
+func TestRampUp(t *testing.T) {
+	log.Println("Start Ramp Up")
+	calls := 0
+	stresser := &stress.Stresser{
+		New: func() stress.Worker {
+			calls++
+			return &slowWorker{
+				delay: time.Second / 2,
+			}
+		},
+		Limit: rate.NewLimiter(10, 10),
+	}
+
+	go stresser.Start()
+	defer stresser.Stop()
+
+	timer := time.NewTimer(5 * time.Second)
+	<-timer.C
+
+	if calls == 1 {
+		t.Errorf("incorrect number of workers created, expected at least more than %d, got %d", 1, calls)
+	}
+}
